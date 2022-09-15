acc1cb17b2f2651eb7d8882ab25e33c2ecf5421a
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2022-09-15 13:52:19 -0400

    Update ReadWriteCSVFile.cs

--- CoreUtility/ReadWriteCSVFile.cs
+++ CoreUtility/ReadWriteCSVFile.cs
@@ -21,3 +21,84 @@ static async Task AddUniqueNumColumn(string headerFilePath)
                 }
             }
         }
+ static void Upload(string csvFile, string tableName, DataTable dt)
+        {
+            string csvData = File.ReadAllText(csvFile);
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    try
+                    {
+                        foreach (string cell in CsvParser(row))
+                        {
+                            dt.Rows[dt.Rows.Count - 1][i] = cell;
+                            i++;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection(_CONN_STRING))
+            {
+                using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                {
+                    sqlBulkCopy.DestinationTableName = tableName;
+                    con.Open();
+                    sqlBulkCopy.WriteToServer(dt);
+                    con.Close();
+                }
+            }
+        }
+
+        static string[] CsvParser(string csvText)
+        {
+            List<string> tokens = new List<string>();
+
+            int last = -1;
+            int current = 0;
+            bool inText = false;
+
+            while (current < csvText.Length)
+            {
+                switch (csvText[current])
+                {
+                    case '"':
+                        inText = !inText; break;
+                    case ',':
+                        if (!inText)
+                        {
+                            tokens.Add(csvText.Substring(last + 1, (current - last)).Trim(' ', ','));
+                            last = current;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                current++;
+            }
+
+            if (last != csvText.Length - 1)
+            {
+                tokens.Add(csvText.Substring(last + 1).Trim());
+            }
+
+            return tokens.ToArray();
+        }
+
+        static DataTable GetMasterSchema()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[3] {
+                new DataColumn("Id",typeof(string)),
+                new DataColumn("Key",typeof(string)),
+                new DataColumn("Data",typeof(string))
+                      });
+            return dt;
+        }
